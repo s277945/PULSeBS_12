c44f8de000402cc72728316c110b83e69cf168ed
s277945 <67461827+s277945@users.noreply.github.com>
2020-11-06 11:23:32 +0100

    P1-26 initial server jwt token management config

--- server/dao.js
+++ server/dao.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const bcrypt = require('bcryptjs');
+
+//check user data for login
+exports.checkUserPwd = function (username, password) {
+    return new Promise((resolve, reject) => {
+        var sql = 'SELECT userId, passwordHash FROM users WHERE userId = ?'; // sql query to select user entry from database        
+        console.log(username);
+        console.log(password);
+        db.get(sql, [username], (err, row) => {
+            if (err) reject(err); // error handling
+            else if (typeof row === 'undefined') reject(new Error('User does not exist')); // no entry found
+            else if (typeof row !== 'undefined') { // username found
+                bcrypt.compare(password, row.passwordHash, (err,res) => { //check password hash
+                    console.log(res);
+                    if (err) reject(err);
+                    else if (!res) reject(new Error('Password is incorrect'));
+                    else resolve(row.userId) // if password is correct return user id
+                })
+            }
+            
+        });
+    });
+};
--- server/server.js
+++ server/server.js
@@ -8,7 +8,9 @@ const dao = require('./dao');
 const morgan = require('morgan'); // logging middleware
 const cors = require('cors');
 const { check, validationResult } = require('express-validator');
+const jwt = require ('express-jwt');
 const moment = require('moment');
+const cookieParser = require('cookie-parser');
 const nodemailer = require('nodemailer'); 
 
 
@@ -16,11 +18,41 @@ const nodemailer = require('nodemailer');
 const app = express();
 const port = 3001;
 
+// Process body content
+app.use(express.json());
+
 // Set-up logging
 app.use(morgan('tiny'));
 
-// Process body content
-app.use(express.json());
+//setup private key
+const jwtSecret = 'aD6c3gT4Fh5gThVd4WwE5Er5gVjGhyT8iOLc2d7W5q1gTdC9vFs5N8ju6Kt7RhIq7Xz8Nt';
+
+//setup JWT token expiration time
+const expireTime = 600; // 10 minutes
+
+//unauthorized access management
+app.use((err, req, res, next) => {
+  if (err.name === 'UnauthorizedError') {
+      res.status(401).json(authErrorObj);
+  }
+});
+
+//login route
+app.post('/api/login', (req, res) => {
+  dao.checkUserPwd(req.body.userName, req.body.password)
+      .then((userId) => {
+          const token = jsonwebtoken.sign({ user: userId }, jwtSecret, { expiresIn: expireTime });
+          res.cookie('token', token, { httpOnly: true, sameSite: true, maxAge: 3000 * expireTime });
+          res.status(200).json(userId).end(); console.log(userId);
+      }).catch( // Delay response in case of wrong user/pass to prevent fast guessing attempts
+          () => new Promise((resolve) => { setTimeout(resolve, 1000) }).then( // 1 second timeout
+              () => {res.status(401).end()}
+          )
+      );
+});
+
+//cookie parsing setup
+app.use(cookieParser());
 
 // Enable cors
 app.use(cors());
