5979a6992f85ea748ee82517ceaa322b0a35eac1
sabat <s266260@studenti.polito.it>
2020-11-07 15:38:28 +0100

    P1-1 Database API for booking P1-5 Database API to remove booking

--- server/dao.js
+++ server/dao.js
@@ -1,11 +1,11 @@
 'use strict';
-
+const db = require('./db');
 const bcrypt = require('bcryptjs');
 
 //check user data for login
 exports.checkUserPwd = function (username, password) {
     return new Promise((resolve, reject) => {
-        var sql = 'SELECT userId, passwordHash FROM users WHERE userId = ?'; // sql query to select user entry from database        
+        var sql = 'SELECT userId, passwordHash FROM users WHERE userId = ?'; // sql query to select user entry from database
         console.log(username);
         console.log(password);
         db.get(sql, [username], (err, row) => {
@@ -19,7 +19,59 @@ exports.checkUserPwd = function (username, password) {
                     else resolve(row.userId) // if password is correct return user id
                 })
             }
-            
+
         });
     });
 };
+
+exports.addSeat=function(userId, lectureId,date){
+    return new Promise((resolve, reject) => {
+        countBooking(lectureId,date).then((row)=>{
+            if(row.Booked<row.Capacity){
+                const sql='INSERT INTO Booking VALUES(?,?,?)';
+                db.run(sql, [lectureId,date,userId], function(err){
+                    if(err)
+                        reject(err);
+                    else
+                        resolve(true);
+                })
+            }
+            else{
+                //waiting list
+                reject(false);
+            }
+
+            }
+            ).catch(throw err);
+
+
+});
+}
+function countBooking(lectureId,date){
+
+    return new Promise((resolve, reject) => {
+        const sql='SELECT COUNT(*) AS Booked, Capacity FROM Booking JOIN Lecture ON Booking.Course_Ref=Lecture.Course_Ref AND Booking.Date_Ref=Lecture.Date WHERE Course_Ref=? AND Date_Ref=?';
+        db.get(sql, [lectureId,date], (err,row)=>{
+            if(err)
+                reject(err);
+            else{
+
+                resolve(row);
+            }
+
+        })
+    })
+}
+
+exports.deleteSeat=function(userId, lectureId,date){
+    return new Promise((resolve, reject) => {
+        const sql='DELETE FROM Booking WHERE Student_Ref=? AND Course_Ref=?AND Date_Ref=?';
+        db.run(sql, [userId, lectureId,date], function(err){
+            if(err)
+                reject(err);
+            else
+                resolve(true);
+        })
+    });
+};
+
