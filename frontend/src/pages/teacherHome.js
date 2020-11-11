a34da4f8e41a4973c5d0a182462fc82472ec292b
Alessandro <s277945@studenti.polito.it>
2020-11-11 15:54:33 +0100

    P1-60 initial commit

--- frontend/src/pages/teacherHome.js
+++ frontend/src/pages/teacherHome.js
@@ -1,19 +1,39 @@
 import React, { Component } from 'react'
 import { TeacherTabSL } from './teacherTabSL'
+import { TeacherNavbar } from './teacherNavbar'
 
 export class TeacherHome extends Component {
-
+    state = {
+        show : 0 //this state variable is used to choose the content to show
+    }
 
     componentDidMount() {
         if(!this.props.context.userName || !this.props.context.userType) this.props.history.push("/"); //open page only if a valid session has been started
 
     }
 
+    setShow = (val) => { //function to set the show variable
+        this.setState({show : val});
+    }
+
+    contentSelect = () => { //function that displays the correct content based on this.state.show
+        if (this.state.show === 0) return (
+            <div>
+                {/*lecture content displayed here*/}
+            </div>
+        )
+        else return (
+            <div>
+
+            </div>
+        )
+    }
 
     render() {
 
         return (
-            <div>Teachers Home <p>Navbar here</p>
+            <div>
+                <TeacherNavbar setShow={this.setShow} history={this.props.history} context={this.props.context}/>
                 <TeacherTabSL></TeacherTabSL>
             </div>
         )
