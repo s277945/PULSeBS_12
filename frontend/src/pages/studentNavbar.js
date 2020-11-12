26d5c8ac8846bcba5dc9770d02ddc8f8e1853247
Alessandro <s277945@studenti.polito.it>
2020-11-12 17:58:21 +0100

    logout bug fix

--- frontend/src/pages/studentNavbar.js
+++ frontend/src/pages/studentNavbar.js
@@ -20,7 +20,16 @@ export class StudentNavbar extends Component {
 
     handleLogout = () => { //function to logout the user
         userIdentity.removeUserSession(this.props.context); //clear user session data
-        axios.post("http://localhost:3001/api/logout"); //close user session
+        //axios.post("http://localhost:3001/api/logout"); //close user session
+        fetch ('http://localhost:3001/api/logout', {// send post request, will delete cookie jwt token
+        method: 'post',
+        credentials: 'include'
+    }).then((r)=>{
+        if(r.status===200) {// check response status
+            console.log("Logout successful "+r.status);
+        }
+        else console.log("Logout error "+r.status);
+        })
         this.props.history.push("/"); //redirect to login page
     }
 
