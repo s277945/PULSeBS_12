2c8b49b482074a81520ddf7ba74fbf5b3524bd26
Alessandro <s277945@studenti.polito.it>
2020-12-03 14:52:26 +0100

    [P1-94] small history background fix

--- frontend/src/pages/teacherTabHist.js
+++ frontend/src/pages/teacherTabHist.js
@@ -18,7 +18,7 @@ export class TeacherTabHist extends Component {
         let courses = [...new Set(this.state.tableData.map(item => item.Course_Ref))];
 
         return (
-            <div><br></br><h1 className="page-title">Historical Data</h1><br></br>
+            <div  className="app-element-background"><br></br><h1 className="page-title">Historical Data</h1><br></br>
                 {courses.map((course) => <div style={{margin: "10px", border: "2px solid gray", borderRadius: "11px", padding: "17px"}}><StatisticsTab course={course}></StatisticsTab></div>)}
             </div>
         )
