de8ea7ad516a8ffda4aa1e69e14433244bcd7ede
Alessandro <s277945@studenti.polito.it>
2020-12-04 02:23:13 +0100

    [P1-150] fix merge

--- frontend/cypress/integration/ManagerTest.js
+++ frontend/cypress/integration/ManagerTest.js
@@ -3,8 +3,11 @@ describe('TEST MANAGER PAGE', function () {
         cy.visit('http://localhost:3000/')
         cy.get('input:first').type('b123456')
         cy.get('input:last').type('scimmia')
+        cy.intercept('POST','/api/login').as('login')
+
         cy.get('.btn.btn-primary')
             .click()
+        cy.wait('@login')
         Cypress.Cookies.preserveOnce('token', 'value')
         Cypress.Cookies.debug(true)
     })
@@ -19,22 +22,26 @@ describe('TEST MANAGER PAGE', function () {
     });
     it('should open accordion of a course', function () {
         cy.get('.accordion')
-            .should('have.length',5)
-            .contains('Data Science')
+            .contains('PDS')
             .should('be.visible')
             .click({force:true})
     });
     it('should change lecture inside select', function () {
         cy.get('.accordion')
-            .contains('Data Science')
+            .contains('PDS')
             .click({force:true})
         cy.get('.collapse.show')
             .should('be.visible')
             .within(()=>{
                 cy.get('select')
-                    .select('DS Les:2')
+                    .select('PDS Les:0')
             })
 
+    });
+    it('should not redirect', function () {
+        cy.visit('http://localhost:3000/')
+        cy.wait(200)
+        cy.location('pathname').should('include','/bookingHome')
     });
     it('should logout', function () {
         cy.get('[data-testid="logout"]')
