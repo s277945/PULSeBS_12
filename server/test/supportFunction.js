8c92a20fb878b171e8bd0521904dbf8e62dfb8ae
sabat <s266260@studenti.polito.it>
2020-12-11 12:58:15 +0100

    P1-146

---
+++ server/test/supportFunction.js
@@ -0,0 +1,60 @@
+const db = require('../src/db');
+
+export function getCourseCapacity(id,date){
+    return new Promise((resolve, reject) => {
+        const sql='SELECT Capacity FROM Lecture WHERE Course_Ref=? AND Date=?';
+        db.get(sql,[id,date],(err,row)=>{
+            if(err)
+                reject(err)
+            else
+                resolve(row);
+        })
+    });
+}
+export function updateCourseCapacity(id,date,capacity){
+    return new Promise((resolve, reject) => {
+        const sql='UPDATE Lecture SET Capacity=? WHERE Course_Ref=? AND Date=?';
+        db.run(sql,[capacity,id,date],function (err){
+            if(err)
+                reject(err);
+            else
+                resolve(true);
+        })
+    });
+}
+export function deleteFromWaiting(user,date,courseRef){
+    return new Promise((resolve, reject) => {
+        const sql='DELETE FROM WaitingList WHERE Course_Ref=? AND Date_Ref=? AND Student_Ref=?';
+        db.run(sql, [courseRef,date,user],function(err){
+            if(err)
+                reject(err);
+            else
+                resolve(true);
+        })
+    })
+}
+
+export function insertDeletedRow(Course_Ref,Name,Date,DateDeadline,Capacity){
+    return new Promise(((resolve, reject) => {
+        const sql='INSERT INTO Lecture(Course_Ref,Name,Date,DateDeadline,Capacity,Type) VALUES(?,?,?,?,?,?)';
+        db.run(sql,[
+            Course_Ref,Name,Date,DateDeadline,Capacity,'p'
+        ],function(err){
+            if(err)
+                reject(err);
+            else
+                resolve(true);
+        });
+    }))
+}
+export function restoreTypeLecture(Course_Ref,Date){
+    return new Promise((resolve, reject) => {
+        const sql='UPDATE Lecture SET Type="p" WHERE Course_Ref=? AND Date=?'
+        db.run(sql,[Course_Ref,Date], function(err){
+            if(err)
+                reject(err);
+            else
+                resolve(true);
+        })
+    })
+}
