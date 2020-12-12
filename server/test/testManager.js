e117c0c3203628a998e2cae2a0f60b2f9028cfa8
sabat <s266260@studenti.polito.it>
2020-12-12 20:47:51 +0100

    P1-146

--- server/test/testManager.js
+++ server/test/testManager.js
@@ -75,6 +75,75 @@ describe('TEST SUITE MANAGER FUNCTION', function () {
                 })
         });
     });
+    describe('SET STUDENT POSITIVE', function () {
+        it('should mark a student as positive by ssn', function () {
+            return chai.request(url)
+                .post('/api/students/:studentSsn')
+                .set('Cookie',cookie)
+                .send()
+                .then(res=>{
+                    expect(res).to.have.status(200)
+                    expect(res.body).to.haveOwnProperty('setAsPositive')
+                    expect(res.body.setAsPositive).to.equals(true)
+                })
+        });
+    });
+    describe('GET LIST OF POSITIVE STUDENTS', function () {
+        it('should return a list of students', function () {
+            return chai.request(url).get('/api/students/positiveStudents')
+                .set('Cookie',cookie)
+                .send()
+                .then(res=>{
+                    expect(res.to.have.status(201))
+                    expect(res.body).to.be.an('array')
+                    expect(res.body).to.be.not.empty
+                    expect(res.body[0]).to.haveOwnProperty('name')
+                    expect(res.body[0].name).to.match(/[a-zA-z]+/)
+                    expect(res.body[0]).to.haveOwnProperty('surname')
+                    expect(res.body[0].surname).to.match(/[a-zA-z]+/)
+                    expect(res.body[0]).to.haveOwnProperty('birthday')
+                    expect(res.body[0].birthday).to.match(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/)
+                })
+        });
+        it('should return single student', function () {
+            return chai.request(url).get('/api/students/TODO')
+                .set('Cookie',cookie)
+                .send()
+                .then(res=>{
+                    expect(res).to.have.status(201)
+                    expect(res.body).to.be.not.empty
+                    expect(res.body).to.haveOwnProperty('name')
+                    expect(res.body.name).to.match(/[a-zA-Z]+/)
+                    expect(res.body).to.haveOwnProperty('surname')
+                    expect(res.body.name).to.match(/[a-zA-Z]+/)
+                    expect(res.body).to.haveOwnProperty('birthday')
+                    expect(res.body.birthday).to.match(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/)
+                    expect(res.body).to.haveOwnProperty('ssn')
+                    expect(res.body.ssn).to.match(/[A-Z]{2}[0-9]{8}/)
+                })
+        });
+        
+    });
+    describe('generate student report of tracking', function () {
+        it('should return list of students that have a contact with a student', function () {
+            return chai.request(url).get('/api/reports/:studentSsn')
+                .set('Cookie',cookie)
+                .send()
+                .then(res=> {
+                    expect(res.to.have.status(201))
+                    expect(res.body).to.be.an('array')
+                    expect(res.body).to.be.not.empty
+                    expect(res.body[0]).to.haveOwnProperty('name')
+                    expect(res.body[0].name).to.match(/[a-zA-z]+/)
+                    expect(res.body[0]).to.haveOwnProperty('surname')
+                    expect(res.body[0].surname).to.match(/[a-zA-z]+/)
+                    expect(res.body[0]).to.haveOwnProperty('ssn')
+                    expect(res.body[0].ssn).to.match(/[A-Z]{2}[0-9]{8}/)
+                    expect(res.body[0]).to.haveOwnProperty('birthday')
+                    expect(res.body[0].birthday).to.match(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/)
+                })
+        });
+    });
     after(async()=>{
         await chai.request(url).post('/api/logout').set('Cookie',cookie).send();
     })
