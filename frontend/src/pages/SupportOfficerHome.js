ca914c5f2d1faafe07bb441fa3b248081b6e8d58
Hilderic <hilderic.bourin@gmail.com>
2020-12-13 19:28:37 +0100

    [P1-165] BE error when uploading student

--- frontend/src/pages/SupportOfficerHome.js
+++ frontend/src/pages/SupportOfficerHome.js
@@ -2,9 +2,11 @@ import React, { useState, useEffect }  from 'react';
 import Navbar  from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import Table from 'react-bootstrap/Table'
-import Form from 'react-bootstrap/Table'
+import Form from 'react-bootstrap/Form'
 import {useAuth} from '../components/Authsystem'
 import Button from 'react-bootstrap/Button'
+import bsCustomFileInput from 'bs-custom-file-input'
+import {uploadStudents} from '../api/api';
 
 export default function BookingManagerHome(){
 
@@ -13,8 +15,8 @@ export default function BookingManagerHome(){
             <NavBarOfficier />
             <h2 className="ml-4 mt-3">Upload data</h2>
 
-            <UploadComponent Name={"Student list"}/>
-            <UploadComponent Name={"Professor list"}/>
+            <UploadComponent Name={"Student list"} listType="student"/>
+            <UploadComponent Name={"Professor list"} listType="professor"/>
 
         </>
     )
@@ -31,7 +33,7 @@ const NavBarOfficier = () => {
     )
 }
 
-const UploadComponent = ({Name}) => {
+const UploadComponent = ({Name, listType}) => {
     return(
         <div className="d-flex justify-content-center mt-5">
             <Table className="w-75 table-bordered">
@@ -46,7 +48,7 @@ const UploadComponent = ({Name}) => {
             <tr>
                 <td>Student.csv</td>
                 <td>2020-09 14:05</td>
-                <td><UploadFileButton /></td>
+                <td><UploadFileButton Name={Name} listType={listType}/></td>
             </tr>
             </tbody>
         </Table>
@@ -54,12 +56,76 @@ const UploadComponent = ({Name}) => {
     )
 }
 
+const UploadFileButton = ({Name, listType}) => {
+    const [files, setFiles] = useState();
 
-const UploadFileButton = () => 
+    useEffect(() => {
+        bsCustomFileInput.init();
+    });
+
+    const sendFile = () => {
+        let file = files[0];
+        if(!file) return;
+
+        const reader = new FileReader();
+
+        reader.onload = e => {
+            let contents = e.target.result;
+      
+            contents = contents.split('\n')
+
+            // TODO : REMOVE ME
+            contents.splice(5)
+
+            // Find headers by list type
+            let headers;
+            switch(listType){
+                case "student":
+                    headers = "userID,Name,Surname,City,email,birthday,ssn".split(',')
+                break; 
+
+                case "professor":
+                    headers = "userID,Name,Surname,City,email,birthday,ssn".split(',')
+                break; 
+            }
+
+            // Create an array 
+            contents.shift()
+            const data = contents.map((line, index) => {
+                const res = {}
+                const values = line.split(",")
+                values.map((val, index2) => {
+                    const header = headers[index2] 
+                    const tmp = {[header]: val}
+                    Object.assign(res, tmp)
+                })
+
+                return res
+            })
+
+
+            uploadStudents(data).then(response => {
+                console.log(response)
+            })
+
+        };
+
+        reader.readAsText(file);
+    }
+
+    return(
     <Form>
-    {/* <Form.File 
-        id="custom-file"
-        label="Custom file input"
-        custom
-    /> */}
+        <div className="d-flex">
+            <Form.File 
+                id="custom-file"
+                label= {Name + " csv list file"}
+                custom
+                onChange={(event) => setFiles(event.target.files)}
+            />
+            <Button className="ml-1" variant="primary" onClick={() => sendFile()}>Send</Button>
+        </div>
     </Form>
+    )
+}
+
+
