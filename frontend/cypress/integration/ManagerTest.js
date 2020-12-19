b3eb7ff5c2d3ff84c66a4fb8c9347c0aa4c9d78c
sabat <s266260@studenti.polito.it>
2020-12-19 19:33:57 +0100

    P1-145

--- frontend/cypress/integration/ManagerTest.js
+++ frontend/cypress/integration/ManagerTest.js
@@ -8,18 +8,35 @@ describe('TEST MANAGER PAGE', function () {
     it('should load correctly manager page', function () {
         cy.location('pathname').should('include', '/bookingHome')
     });
-    it('should render', function () {
+    it('should render correctly', function () {
         cy.get('nav')
             .should('include.text', 'PULSeBS - SystemActivity')
-
+        cy.get('.accordion')
+            .should('include.text','Data Science')
+            .should('have.length',1)
 
     });
 
     it('should open accordion of a course', function () {
         cy.get('.accordion')
-            .eq(0)
             .should('be.visible')
             .click()
+        cy.get('.card-body')
+            .should('be.visible')
+            .within(()=>{
+                cy.get('h5').eq(0).should('have.text','Total Information')
+                cy.get('h5').eq(1).should('have.text','Information by Lecture')
+                cy.get('tbody>tr').then(($lis)=>{
+                    expect($lis.eq(0)).to.contain('6')
+                    expect($lis.eq(0)).to.contain('192')
+                    expect($lis.eq(0)).to.contain('45')
+                    expect($lis.eq(0)).to.contain('147')
+                    expect($lis.eq(1)).to.contain('70')
+                    expect($lis.eq(1)).to.contain('23')
+                    expect($lis.eq(1)).to.contain('47')
+
+                })
+            })
         cy.wait(100)
     });
     it('should change lecture inside select', function () {
@@ -31,7 +48,16 @@ describe('TEST MANAGER PAGE', function () {
             .should('be.visible')
             .within(() => {
                 cy.get('select')
-                    .first()
+                    .select('DS Les:2')
+
+            })
+        cy.get('.card-body').should('be.visible')
+            .within(()=>{
+                cy.get('tbody>tr').eq(1).within(()=>{
+                    cy.get('td').eq(0).should('contains.text','30')
+                    cy.get('td').eq(1).should('contains.text','7')
+                    cy.get('td').eq(2).should('contains.text','23')
+                })
             })
 
     });
@@ -74,6 +100,7 @@ describe('TEST MANAGER PAGE', function () {
                 cy.wait(200)
 
             })
+        cy.get('tbody>tr').should('have.length',5)
 
     });
     it('should close correctly popup', function () {
@@ -85,6 +112,13 @@ describe('TEST MANAGER PAGE', function () {
             .click({ force: true });
     });
     it('should not add a positive student if the ssn does not exists', function () {
+        cy.server()
+        cy.route({
+            method:'GET',
+            url:'/api/students/*',
+            status:500,
+            response:{}
+        }).as('student')
         cy.get('[data-testid="report"]')
             .click()
         cy.get('[data-testid="addSSN"]').should('have.text', 'Add New Student')
@@ -95,20 +129,35 @@ describe('TEST MANAGER PAGE', function () {
                     .should('be.visible')
                     .type('TODO')
                 cy.get('[data-testid="search"]').click()
+                cy.wait('@student')
             })
             .should('contain.text', 'Student not found');
     });
-        it('should generate a report', function () {
+        it('should generate a report empty', function () {
+            cy.server()
+            cy.route({
+                method:'GET',
+                url:'/api/reports/*',
+                status:200,
+                response:[]
+            }).as('report')
             cy.get('[data-testid="report"]')
                 .click()
-            cy.get('tbody>tr').eq(1)
+            cy.get('tbody>tr').eq(0)
                 .within(()=>{
                     cy.get('.btn.btn-primary')
                         .click()
                 })
-            cy.wait(100)
+            cy.wait('@report')
         });
         it('should not generate a report if a student is not enrolled in any lectures', function () {
+            cy.server()
+            cy.route({
+                method:'GET',
+                url:'/api/reports/*',
+                status:500,
+                response:{}
+            }).as('report')
             cy.get('[data-testid="report"]')
                 .click()
             cy.get('tbody>tr').eq(2)
@@ -116,10 +165,11 @@ describe('TEST MANAGER PAGE', function () {
                     cy.get('.btn.btn-primary')
                         .click()
                 })
+            cy.wait('@report')
         });
         it('should generate correctly a report', function () {
             cy.get('[data-testid="report"]').click()
-            cy.get('tbody>tr').eq(0)
+            cy.get('tbody>tr').eq(1)
                 .within(()=>{
                     cy.get('.btn.btn-primary')
                         .click()
