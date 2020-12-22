7df86b14cddf6dea5d50e30da574f4c30a0047d8
sabat <s266260@studenti.polito.it>
2020-12-22 22:29:00 +0100

    P1-193

--- frontend/src/pages/SupportOfficerSchedule.js
+++ frontend/src/pages/SupportOfficerSchedule.js
@@ -4,7 +4,6 @@ import Button from 'react-bootstrap/Button'
 import{getAllSchedule,setNewSchedule} from "../api/api";
 import Modal from 'react-bootstrap/Modal'
 import Form from 'react-bootstrap/Form'
-import Col from 'react-bootstrap/Col'
 import TimePicker from 'react-bootstrap-time-picker';
 import moment from "moment";
 
