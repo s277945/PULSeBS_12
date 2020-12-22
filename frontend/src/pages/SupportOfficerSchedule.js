ea22e32df5ce85e499cd595e27399aa15efec166
sabat <s266260@studenti.polito.it>
2020-12-22 11:44:04 +0100

    P1-193 first commit

---
+++ frontend/src/pages/SupportOfficerSchedule.js
@@ -0,0 +1,72 @@
+import React, {Component} from 'react'
+import Table from 'react-bootstrap/Table'
+import Button from 'react-bootstrap/Button'
+import Modal from 'react-bootstrap/Modal'
+
+export class SupportOfficerSchedule extends Component{
+    state={
+        listSchedule:[],
+        modal:false
+    }
+    componentDidMount() {
+        this.updateSchedule();
+    }
+    updateSchedule=()=>{
+        getAllSchedule()
+            .then(res=>{
+                this.setState({ listSchedule: res.data });
+            })
+            .catch(err=>{
+                console.log(err);
+            })
+    }
+    renderSchedule=()=>{
+        //Fill the body of the table from fetched values
+        let table = [];
+        let k = 0;
+        this.state.listSchedule.forEach(row => {
+            table.push(<tr key={k++}>
+                <td>{row.courseId}</td>
+                <td>{row.courseName}</td>
+                <td>{row.room}</td>
+                <td>{row.day}</td>
+                <td>{row.seats}</td>
+                <td>{row.time}</td>
+                <td style={{ display: "flex", justifyContent: "flex-start" }}><Button style={{ marginLeft: "5px" }} data-testid={"showReport_" + k++} onClick={(e) => { e.preventDefault();}}>Modify Schedule</Button></td>
+            </tr>)
+        });
+        return(
+            <Table striped bordered hover style={{ backgroundColor: "#fff" , width: "98%", margin: "auto"}}>
+                <thead>
+                <tr>
+                    <th>Course Name</th>
+                    <th>CourseID</th>
+                    <th>Room</th>
+                    <th>Day</th>
+                    <th>Seats</th>
+                    <th>Time</th>
+                    <th>Update</th>
+                </tr>
+                </thead>
+                <tbody data-testid={"listTabSL"}>
+                {table}
+                </tbody>
+            </Table>
+        )
+    }
+    renderModalSchedule=()=>{
+
+    }
+    render() {
+        return(
+            <div ><br></br><h1 className="page-title">Update schedule lectures</h1><br></br>
+                <br/>
+                <Button data-testid="addSSN" style={{ marginLeft: "1vw", marginBottom: "13px" }} onClick={(e) => { e.preventDefault(); this.setState({ modal: true }) }}>Add New Student</Button>
+                {this.renderSchedule()}
+
+                {this.renderModalSchedule()}
+            </div>
+        )
+    }
+
+}
