731cec05bd1cab2b7c8409111d3c712f2186c5c1
Alessandro <s277945@studenti.polito.it>
2020-12-22 17:47:57 +0100

    [P1-192] select all button

--- frontend/src/pages/teacherTabSL.js
+++ frontend/src/pages/teacherTabSL.js
@@ -115,6 +115,10 @@
      }
      
      renderPastLectures() {
+        const setChecks=(elem)=>{elem.setState({ 
+            modalTableData: this.state.modalTableData.map(e => {
+                return { userId: e.userId, name: e.name, surname: e.surname, attendance: e.attendance, checked: true }
+        })})}
         const resetChecks=(elem)=>{elem.setState({ 
             modalTableData: this.state.modalTableData.map(e => {
                 return { userId: e.userId, name: e.name, surname: e.surname, attendance: e.attendance, checked: false }
@@ -208,9 +212,12 @@
                              </Table> :
                              <div />}
                              {this.state.modalLecture&&moment().diff(moment(this.state.modalLecture.Date), 'days') <= 1 ?
-                             <div style={{display:"flex", justifyContent:"flex-end"}}>
-                                <Button style={{marginRight: "10px"}} disabled={this.state.modalTableData.filter(e=>e.checked).length<1} onClick={(e)=>{e.preventDefault(); setPresence(this);}}>Mark as present</Button>
-                                <Button variant="secondary" disabled={this.state.modalTableData.filter(e=>e.checked).length<1} onClick={(e)=>{e.preventDefault(); resetChecks(this);}}>Reset selection</Button>
+                             <div style={{display:"flex", justifyContent:"space-between"}}>
+                                 <div>
+                                    <Button style={{marginRight: "10px"}} variant="danger" disabled={this.state.modalTableData.filter(e=>e.checked).length<1} onClick={(e)=>{e.preventDefault(); resetChecks(this);}}>Reset selection</Button>
+                                    <Button variant="secondary" disabled={this.state.modalTableData.filter(e=>e.checked).length===this.state.modalTableData.filter(e=>e.attendance===0).length} onClick={(e)=>{e.preventDefault(); setChecks(this);}}>Select all</Button>
+                                </div>
+                                    <Button disabled={this.state.modalTableData.filter(e=>e.checked).length<1} onClick={(e)=>{e.preventDefault(); setPresence(this);}}>Mark as present</Button>
                              </div>
                              :<div/>}
                      </Modal.Body>
