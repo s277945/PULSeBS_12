870b81916e1433e2ea3e3bacab7f1c013e3e966b
sabat <s266260@studenti.polito.it>
2020-12-22 13:48:34 +0100

    P1-193 added modal

--- frontend/src/pages/SupportOfficerSchedule.js
+++ frontend/src/pages/SupportOfficerSchedule.js
@@ -8,7 +8,8 @@ import moment from "moment";
 export class SupportOfficerSchedule extends Component{
     state={
         listSchedule:[],
-        modal:false
+        modal:false,
+        elemModal:[]
     }
     componentDidMount() {
         this.updateSchedule();
@@ -22,44 +23,25 @@ export class SupportOfficerSchedule extends Component{
                 console.log(err);
             })
     }
-    renderSchedule=()=>{
-        //Fill the body of the table from fetched values
-        let table = [];
-        let k = 0;
-        this.state.listSchedule.forEach(row => {
-            table.push(<tr key={k++}>
-                <td>{row.courseId}</td>
-                <td>{row.courseName}</td>
-                <td>{row.room}</td>
-                <td>{row.day}</td>
-                <td>{row.seats}</td>
-                <td>{row.time}</td>
-                <td style={{ display: "flex", justifyContent: "flex-start" }}><Button style={{ marginLeft: "5px" }} data-testid={"showReport_" + k++} onClick={(e) => { e.preventDefault();this.setState({ modal: true });this.renderModalSchedule(row)}}>Modify Schedule</Button></td>
+
+    renderModalSchedule=()=>{
+        console.log(JSON.stringify());
+        console.log("modal show: "+this.state.modal);
+        let list=[];
+
+        if(this.state.elemModal!=undefined){
+            let obj=this.state.elemModal;
+
+            list.push(<tr>
+                <td>{obj.courseId}</td>
+                <td>{obj.courseName}</td>
+                <td>{obj.room}</td>
+                <td>{obj.day}</td>
+                <td>{obj.seats}</td>
+                <td>{obj.time}</td>
             </tr>)
-        });
-        return(
-            <Table striped bordered hover style={{ backgroundColor: "#fff" , width: "98%", margin: "auto"}}>
-                <thead>
-                <tr>
-                    <th>CourseID</th>
-                    <th>CourseName</th>
-                    <th>Room</th>
-                    <th>Day</th>
-                    <th>Seats</th>
-                    <th>Time</th>
-                    <th>Update</th>
-                </tr>
-                </thead>
-                <tbody data-testid={"listTabSL"}>
-                {table}
-                </tbody>
-            </Table>
-        )
-    }
-    renderModalSchedule=(row)=>{
-        console.log(JSON.stringify(row));
-        console.log("modal show: "+this.state.modal)
-        let obj=row;
+        }
+
         return(
             <Modal data-testid="modalSSN" show={this.state.modal} onHide={() => { /* When the modal is closed clear the response message and the searched student */ this.setState({ modal: false });}}>
                 <Modal.Header data-testid={"close"} closeButton>
@@ -87,18 +69,10 @@ export class SupportOfficerSchedule extends Component{
                                 <th>Day</th>
                                 <th>Seats</th>
                                 <th>Time</th>
-                                <th>Update</th>
                             </tr>
                             </thead>
                             <tbody data-testid={"modalScheduleTab"}>
-                                <tr>
-                                    <td>{row.courseId}</td>
-                                    <td>{row.courseName}</td>
-                                    <td>{row.room}</td>
-                                    <td>{row.day}</td>
-                                    <td>{row.seats}</td>
-                                    <td>{row.time}</td>
-                                </tr>
+                            {list}
                             </tbody>
                         </Table>
                     </div>
@@ -106,12 +80,47 @@ export class SupportOfficerSchedule extends Component{
             </Modal>
         )
     }
+    renderSchedule=()=>{
+        //Fill the body of the table from fetched values
+        let table = [];
+        let k = 0;
+        this.state.listSchedule.forEach(row => {
+            table.push(<tr key={k++}>
+                <td>{row.courseId}</td>
+                <td>{row.courseName}</td>
+                <td>{row.room}</td>
+                <td>{row.day}</td>
+                <td>{row.seats}</td>
+                <td>{row.time}</td>
+                <td style={{ display: "flex", justifyContent: "flex-start" }}><Button style={{ marginLeft: "5px" }} data-testid={"showReport_" + k++} onClick={(e) => { e.preventDefault();this.setState({ modal:true,elemModal:row});}}>Modify Schedule</Button></td>
+            </tr>)
+        });
+        return(
+            <Table striped bordered hover style={{ backgroundColor: "#fff" , width: "98%", margin: "auto"}}>
+                <thead>
+                <tr>
+                    <th>CourseID</th>
+                    <th>CourseName</th>
+                    <th>Room</th>
+                    <th>Day</th>
+                    <th>Seats</th>
+                    <th>Time</th>
+                    <th>Update</th>
+                </tr>
+                </thead>
+                <tbody data-testid={"listTabSL"}>
+                {table}
+                </tbody>
+            </Table>
+        )
+    }
+
     render() {
         return(
             <div ><br></br><h1 className="page-title">Update schedule lectures</h1><br></br>
                 <br/>
                 {this.renderSchedule()}
-
+                {this.renderModalSchedule()}
             </div>
         )
     }
