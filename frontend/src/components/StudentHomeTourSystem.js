bf54d98a00b48788308bef1cca1b15e60afa2185
Hilderic <hilderic.bourin@gmail.com>
2021-01-07 16:03:51 +0100

    [P1-22] Mocking data for tour

--- frontend/src/components/StudentHomeTourSystem.js
+++ frontend/src/components/StudentHomeTourSystem.js
@@ -30,8 +30,32 @@ const steps = [
         selector: '[tour-selec="navbar"]',
         content: 'You can change between lectures/calendar view in the nav bar',
     },
+    {
+        selector: '[tour-selec="today-lecture"]',
+        content: 'Here will be the lectures for today',
+    },
+    {
+        selector: '[tour-selec="course-card"]',
+        content: 'Here you can foud lectures regrouped by courses',
+    },
 ];
 
+
+
+export const getTourState = (state) =>{
+    return{
+        show : 0, //This state variable is used to choose the content to show. (0 : table, 1: calendar)
+        courses: tourCourses,
+        lectures: tourLecture,
+        togglecourse: null,
+        modal: {show: 0, lecture: null, message: null}, //this object contains all modal state variables
+        popup: {show: 0, lecture: {Name: "", Date: ""}, message: null}// this object contains all popup related variables
+    }
+}
+
+const today = new Date();
+const tomorrow = new Date(today)
+tomorrow.setDate(tomorrow.getDate() + 1)
 
         // BookedSeats: 1
         // Capacity: 100
@@ -42,10 +66,31 @@ const steps = [
         // Name: "WA Les:3"
         // Type: "p"
         // alreadyBooked: true
-export const tourLecture = [
+
+
+const tourLecture = [
     {
         Name: "Lecture1",
         Type: "p",
-        Date: new Date()
-    }
+        Date: today,
+        DateDeadline: tomorrow,
+        Course_Ref: "C0123",
+        Capacity: 50,
+        BookedSeats: 5
+    },
+    {
+        Name: "Lecture2",
+        Type: "p",
+        Date: tomorrow,
+        Capacity: 50,
+        BookedSeats: 50,
+        Course_Ref: "C0123"
+    },
+]
+
+const tourCourses = [
+{
+    CourseID: "C0123",
+    Name: "Software Engineering II"
+}
 ]
